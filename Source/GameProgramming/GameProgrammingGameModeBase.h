0f466ee509b82b584600abbe1b6526917cb098bb
2445968904 <44425297+2445968904@users.noreply.github.com>
2024-09-25 13:49:38 +0800

    Init

---
+++ Source/GameProgramming/GameProgrammingGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameProgrammingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPROGRAMMING_API AGameProgrammingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GameProgrammingEditor.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class GameProgrammingEditorTarget : TargetRules
+{
+	public GameProgrammingEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
+		ExtraModuleNames.AddRange( new string[] { "GameProgramming" } );
+	}
+}
